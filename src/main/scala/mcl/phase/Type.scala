8983c2207a562d08ce2c8432a65d2501f7351d75
intsuc <intsuc@gmail.com>
2021-02-23 22:19:06 +0900

    Implement substitute and normalize

--- src/main/scala/mcl/phase/Type.scala
+++ src/main/scala/mcl/phase/Type.scala
@@ -2,20 +2,60 @@ package mcl.phase
 
 import mcl.ast.Source.Term as S
 
-// TODO: normalization
-// TODO: substitution
+// TODO: normalization-by-evaluation
 object Type extends (S => Option[S]):
   type Ctx = Map[Int, S]
 
-  private def normalize(term: S): S = ???
+  private def substitute(term: S)(using subst: Map[Int, S]): S = term match
+    case S.Fun(id, domain, codomain) =>
+      S.Fun(id, substitute(domain), substitute(codomain))
 
-  private def substitute(term: S, subst: Map[Int, S]): S = ???
+    case S.Abs(id, body) =>
+      S.Abs(id, substitute(body))
 
-  private def inferConstrained[T <: S](term: S)(using ctx: Ctx): Option[T] =
+    case S.App(operator, operand) =>
+      S.App(substitute(operator), substitute(operand))
+
+    case S.Var(id) if subst contains id =>
+      subst(id)
+
+    case S.Anno(target, annotation) =>
+      S.Anno(substitute(target), substitute(annotation))
+
+    case term =>
+      term
+
+  private def normalize(term: S): S = term match
+    case S.Fun(id, domain, codomain) =>
+      S.Fun(id, normalize(domain), normalize(codomain))
+
+    case S.Abs(id, body) =>
+      S.Abs(id, normalize(body))
+
+    case S.App(operator, operand) =>
+      normalize(operator) match
+      case S.Abs(id, body) =>
+        normalize(substitute(body)(using Map(id -> normalize(operand))))
+      case operator =>
+        S.App(operator, normalize(operand))
+
+    case S.Anno(target, annotation) =>
+      S.Anno(normalize(target), normalize(annotation))
+
+    case term =>
+      term
+
+  private def inferType(term: S)(using ctx: Ctx): Option[S] =
     for
       typ <- infer(term)
-      result = normalize(typ) if result.isInstanceOf[T]
-    yield result.asInstanceOf[T]
+      result @ S.Type(_) = normalize(typ)
+    yield result
+
+  private def inferFun(term: S)(using ctx: Ctx): Option[S] =
+    for
+      typ <- infer(term)
+      result @ S.Fun(_, _, _) = normalize(typ)
+    yield result
 
   private def infer(term: S)(using ctx: Ctx): Option[S] = term match
     case S.Type(level) =>
@@ -23,15 +63,15 @@ object Type extends (S => Option[S]):
 
     case S.Fun(id, domain, codomain) =>
       for
-        S.Type(domainLevel) <- inferConstrained[S.Type](domain)
-        S.Type(codomainLevel) <- inferConstrained[S.Type](codomain)(using ctx + (id -> domain))
+        S.Type(domainLevel) <- inferType(domain)
+        S.Type(codomainLevel) <- inferType(codomain)(using ctx + (id -> domain))
       yield S.Type(domainLevel max codomainLevel)
 
     case S.App(operator, operand) =>
       for
-        S.Fun(id, domain, codomain) <- inferConstrained[S.Fun](operator)
+        S.Fun(id, domain, codomain) <- inferFun(operator)
         _ <- check(operand, domain)
-      yield substitute(codomain, Map(id -> operand))
+      yield substitute(codomain)(using Map(id -> operand))
 
     case S.Var(id) =>
       ctx.get(id)
@@ -45,7 +85,7 @@ object Type extends (S => Option[S]):
   private def check(term: S, typ: S)(using ctx: Ctx): Option[Unit] = (term, typ) match
     case (S.Abs(id, body), S.Fun(_, domain, codomain)) =>
       for
-        _ <- inferConstrained[S.Type](domain)
+        _ <- inferType(domain)
         _ <- check(body, codomain)(using ctx + (id -> domain))
       yield ()
 
