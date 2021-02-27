ca091f7f2d97ca1ec23251596ae92023d9e1aba9
intsuc <intsuc@gmail.com>
2021-02-28 02:49:34 +0900

    Implement normalization by evaluation

--- src/main/scala/mcl/ast/Source.scala
+++ src/main/scala/mcl/ast/Source.scala
@@ -1,10 +1,12 @@
 package mcl.ast
 
+import mcl.Sym
+
 object Source with
   enum Term with
     case Type(level: Int)
-    case Fun(id: Int, domain: Term, codomain: Term)
-    case Abs(id: Int, domain: Term, body: Term)
+    case Fun(id: Sym, domain: Term, codomain: Term)
+    case Abs(id: Sym, domain: Term, body: Term)
     case App(operator: Term, operand: Term)
-    case Var(id: Int)
-    case Ind(id: Int, constructors: Seq[Int], body: Term)
+    case Var(id: Sym)
+    case Ind(id: Sym, constructors: Seq[Sym], body: Term)
--- src/main/scala/mcl/phase/Type.scala
+++ src/main/scala/mcl/phase/Type.scala
@@ -1,12 +1,44 @@
 package mcl.phase
 
+import mcl.Sym
 import mcl.ast.Source.Term as S
 
-// TODO: normalization-by-evaluation
 object Type extends (S => Option[S]):
-  type Ctx = Map[Int, S]
+  private enum Semantics with
+    case Abs(fun: Semantics => Semantics)
+    case Acc(head: S, tail: Seq[Semantics])
 
-  private def substitute(term: S)(using subst: Map[Int, S]): S = term match
+    def apply(operand: Semantics): Semantics = this match
+      case Abs(fun) =>
+        fun(operand)
+
+      case Acc(id, tail) =>
+        Acc(id, tail :+ operand)
+
+  private def reflect(term: S)(using ctx: Map[Sym, Semantics]): Semantics = term match
+    case S.Abs(id, _, body) =>
+      Semantics.Abs(parameter => reflect(body)(using ctx + (id -> parameter)))
+
+    case S.App(operator, operand) =>
+      reflect(operator)(reflect(operand))
+
+    case S.Var(id) if ctx contains id =>
+      ctx(id)
+
+    case term =>
+      Semantics.Acc(term, Seq.empty)
+
+  private def reify(semantics: Semantics): S = semantics match
+    case Semantics.Abs(fun) =>
+      val id = Sym.fresh()
+      S.Abs(id, S.Type(-1), reify(fun(Semantics.Acc(S.Var(id), Seq.empty))))
+
+    case Semantics.Acc(head, tail) =>
+      tail.foldLeft(head)((operator, operand) => S.App(operator, reify(operand)))
+
+  private def normalize(term: S): S = reify(reflect(term)(using Map.empty))
+
+  private def substitute(term: S)(using subst: Map[Sym, S]): S = term match
     case S.Fun(id, domain, codomain) =>
       S.Fun(id, substitute(domain), substitute(codomain))
 
@@ -25,39 +57,19 @@ object Type extends (S => Option[S]):
     case term =>
       term
 
-  private def normalize(term: S): S = term match
-    case S.Fun(id, domain, codomain) =>
-      S.Fun(id, normalize(domain), normalize(codomain))
-
-    case S.Abs(id, domain, body) =>
-      S.Abs(id, normalize(domain), normalize(body))
-
-    case S.App(operator, operand) =>
-      normalize(operator) match
-      case S.Abs(id, domain, body) =>
-        normalize(substitute(body)(using Map(id -> normalize(operand))))
-      case operator =>
-        S.App(operator, normalize(operand))
-
-    case S.Ind(id, constructors, body) =>
-      S.Ind(id, constructors, normalize(body))
-
-    case term =>
-      term
-
-  private def inferType(term: S)(using ctx: Ctx): Option[S] =
+  private def inferType(term: S)(using ctx: Map[Sym, S]): Option[S] =
     for
       typ <- infer(term)
       result @ S.Type(_) = normalize(typ)
     yield result
 
-  private def inferFun(term: S)(using ctx: Ctx): Option[S] =
+  private def inferFun(term: S)(using ctx: Map[Sym, S]): Option[S] =
     for
       typ <- infer(term)
       result @ S.Fun(_, _, _) = normalize(typ)
     yield result
 
-  private def infer(term: S)(using ctx: Ctx): Option[S] = term match
+  private def infer(term: S)(using ctx: Map[Sym, S]): Option[S] = term match
     case S.Type(level) =>
       Some(S.Type(level + 1))
 
@@ -85,7 +97,7 @@ object Type extends (S => Option[S]):
     case S.Ind(id, constructors, body) =>
       infer(body)(using ctx + (id -> S.Type(0)) ++ constructors.map(_ -> S.Var(id)))
 
-  private def check(term: S, typ: S)(using ctx: Ctx): Boolean =
+  private def check(term: S, typ: S)(using ctx: Map[Sym, S]): Boolean =
     infer(term).map(_ == typ).getOrElse(false)
 
   def apply(source: S): Option[S] =
