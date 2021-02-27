7cecd59d51bcc21dd77149e9516253fd379ef345
intsuc <intsuc@gmail.com>
2021-02-28 06:01:05 +0900

    Check α-equivalence

--- src/main/scala/mcl/phase/Type.scala
+++ src/main/scala/mcl/phase/Type.scala
@@ -5,19 +5,36 @@ import mcl.ast.Source.Term as S
 
 object Type extends (S => Option[S]):
   private enum Semantics with
-    case Abs(fun: Semantics => Semantics)
-    case Acc(head: S, tail: Seq[Semantics])
+    case Abs(domain: Semantics, abs: Semantics => Semantics, reifier: (Sym, S, S) => S)
+    case Acc(head: S, tail: Seq[Semantics] = Seq.empty)
 
     def apply(operand: Semantics): Semantics = this match
-      case Abs(fun) =>
-        fun(operand)
+      case Abs(_, abs, _) =>
+        abs(operand)
 
       case Acc(id, tail) =>
         Acc(id, tail :+ operand)
 
-  private def reflect(term: S)(using ctx: Map[Sym, Semantics]): Semantics = term match
-    case S.Abs(id, _, body) =>
-      Semantics.Abs(parameter => reflect(body)(using ctx + (id -> parameter)))
+    def === (that: Semantics): Boolean = (this, that) match
+      case (Abs(domain1, abs1, reifier1), Abs(domain2, abs2, reifier2)) =>
+        val dummy = Acc(S.Var(Sym.fresh()))
+        reifier1 == reifier2 && domain1 === domain2 && abs1(dummy) === abs2(dummy)
+
+      case (Acc(head1, tail1), Acc(head2, tail2)) =>
+        head1 == head2 && tail1.zip(tail2).forall(_ === _)
+
+      case _ =>
+        false
+
+  private val reifierFun = S.Fun.apply
+  private val reifierAbs = S.Abs.apply
+
+  private def reflect(term: S)(using ctx: Map[Sym, Semantics] = Map.empty): Semantics = term match
+    case S.Fun(id, domain, codomain) =>
+      Semantics.Abs(reflect(domain), parameter => reflect(codomain)(using ctx + (id -> parameter)), reifierFun)
+
+    case S.Abs(id, domain, body) =>
+      Semantics.Abs(reflect(domain), parameter => reflect(body)(using ctx + (id -> parameter)), reifierAbs)
 
     case S.App(operator, operand) =>
       reflect(operator)(reflect(operand))
@@ -26,17 +43,17 @@ object Type extends (S => Option[S]):
       ctx(id)
 
     case term =>
-      Semantics.Acc(term, Seq.empty)
+      Semantics.Acc(term)
 
   private def reify(semantics: Semantics): S = semantics match
-    case Semantics.Abs(fun) =>
+    case Semantics.Abs(domain, abs, reifier) =>
       val id = Sym.fresh()
-      S.Abs(id, S.Type(-1), reify(fun(Semantics.Acc(S.Var(id), Seq.empty))))
+      reifier(id, reify(domain), reify(abs(Semantics.Acc(S.Var(id)))))
 
     case Semantics.Acc(head, tail) =>
       tail.foldLeft(head)((operator, operand) => S.App(operator, reify(operand)))
 
-  private def normalize(term: S): S = reify(reflect(term)(using Map.empty))
+  private def normalize(term: S): S = (reify compose reflect)(term)
 
   private def substitute(term: S)(using subst: Map[Sym, S]): S = term match
     case S.Fun(id, domain, codomain) =>
@@ -98,7 +115,7 @@ object Type extends (S => Option[S]):
       infer(body)(using ctx + (id -> S.Type(0)) ++ constructors.map(_ -> S.Var(id)))
 
   private def check(term: S, typ: S)(using ctx: Map[Sym, S]): Boolean =
-    infer(term).map(_ == typ).getOrElse(false)
+    infer(term).map(reflect(_) === reflect(typ)).getOrElse(false)
 
   def apply(source: S): Option[S] =
     for _ <- infer(source)(using Map.empty) yield source
