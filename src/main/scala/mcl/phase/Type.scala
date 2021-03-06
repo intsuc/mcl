16d7a99787cfd654fb53326db17a6ba417562b1b
intsuc <intsuc@gmail.com>
2021-03-06 20:12:58 +0900

    Use explicit constructors for semantics

--- src/main/scala/mcl/phase/Type.scala
+++ src/main/scala/mcl/phase/Type.scala
@@ -6,59 +6,68 @@ import mcl.ast.Source.Exp
 object Type extends (Exp => Option[Exp]):
   private enum Sem with
     case Type(level: Int)
-    case Abs(domain: Sem, abs: Sem => Sem, typ: Boolean)
-    case Acc(head: Exp, tail: Seq[Sem] = Seq.empty)
-
-  extension (operator: Sem) private def apply(operand: Sem): Sem = operator match
-    case Sem.Abs(_, abs, _) =>
-      abs(operand)
-
-    case Sem.Acc(head, tail) =>
-      Sem.Acc(head, tail :+ operand)
-
-    case _ =>
-      throw IllegalStateException()
+    case Fun(domain: Sem, fun: Sem => Sem)
+    case Abs(domain: Sem, abs: Sem => Sem)
+    case App(operator: Sem, operand: Sem)
+    case Var(id: Sym)
 
   private def reflect(exp: Exp, env: Map[Sym, Sem] = Map.empty): Sem = exp match
     case Exp.Type(level) =>
       Sem.Type(level)
 
     case Exp.Fun(id, domain, codomain) =>
-      Sem.Abs(reflect(domain, env), sem => reflect(codomain, env + (id -> sem)), true)
+      Sem.Fun(reflect(domain, env), sem => reflect(codomain, env + (id -> sem)))
 
     case Exp.Abs(id, domain, body) =>
-      Sem.Abs(reflect(domain, env), sem => reflect(body, env + (id -> sem)), false)
+      Sem.Abs(reflect(domain, env), sem => reflect(body, env + (id -> sem)))
 
     case Exp.App(operator, operand) =>
-      reflect(operator, env)(reflect(operand, env))
+      (reflect(operator, env), reflect(operand, env)) match
+      case (Sem.Abs(_, abs), operand) => abs(operand)
+      case (operator, operand) => Sem.App(operator, operand)
 
-    case Exp.Var(id) if env contains id =>
+    case Exp.Var(id) =>
       env(id)
-
-    case exp =>
-      Sem.Acc(exp)
 
   private def reify(sem: Sem): Exp = sem match
     case Sem.Type(level) =>
       Exp.Type(level)
 
-    case Sem.Abs(domain, abs, typ) =>
+    case Sem.Fun(domain, fun) =>
       val id = Sym.fresh()
-      val constructor = if typ then Exp.Fun.apply else Exp.Abs.apply
-      constructor(id, reify(domain), reify(abs(Sem.Acc(Exp.Var(id)))))
+      Exp.Abs(id, reify(domain), reify(fun(Sem.Var(id))))
 
-    case Sem.Acc(head, tail) =>
-      tail.foldLeft(head)((operator, operand) => Exp.App(operator, reify(operand)))
+    case Sem.Abs(domain, abs) =>
+      val id = Sym.fresh()
+      Exp.Abs(id, reify(domain), reify(abs(Sem.Var(id))))
+
+    case Sem.App(operator, operand) =>
+      Exp.App(reify(operator), reify(operand))
+
+    case Sem.Var(id) =>
+      Exp.Var(id)
 
   extension (sem1: Sem) private def === (sem2: Sem): Boolean = (sem1, sem2) match
-    case (Sem.Abs(domain1, abs1, typ1), Sem.Abs(domain2, abs2, typ2)) if typ1 == typ2 =>
+    case (Sem.Type(level1), Sem.Type(level2)) =>
+      level1 == level2
+
+    case (Sem.Fun(domain1, fun1), Sem.Fun(domain2, fun2)) =>
       domain1 === domain2 && {
-        val dummy = Sem.Acc(Exp.Var(Sym.fresh()))
+        val dummy = Sem.Var(Sym.fresh())
+        fun1(dummy) === fun2(dummy)
+      }
+
+    case (Sem.Abs(domain1, abs1), Sem.Abs(domain2, abs2)) =>
+      domain1 === domain2 && {
+        val dummy = Sem.Var(Sym.fresh())
         abs1(dummy) === abs2(dummy)
       }
 
-    case (Sem.Acc(head1, tail1), Sem.Acc(head2, tail2)) =>
-      head1 == head2 && (tail1 zip tail2).forall(_ === _)
+    case (Sem.App(operator1, operand1), Sem.App(operator2, operand2)) =>
+      operator1 === operator2 && operand1 === operand2
+
+    case (Sem.Var(id1), Sem.Var(id2)) =>
+      id1 == id2
 
     case _ =>
       false
@@ -74,7 +83,7 @@ object Type extends (Exp => Option[Exp]):
   private def inferFun(exp: Exp)(using Map[Sym, Typ]): Option[Typ] =
     for
       typ <- infer(exp)
-      result @ Sem.Abs(_, _, true) = typ
+      result @ Sem.Fun(_, _) = typ
     yield result
 
   private def infer(exp: Exp)(using ctx: Map[Sym, Typ]): Option[Typ] = exp match
@@ -92,13 +101,13 @@ object Type extends (Exp => Option[Exp]):
         _ <- inferType(domain)
         domain <- Some(reflect(domain))
         codomain <- infer(body)(using ctx + (id -> domain))
-      yield Sem.Abs(domain, sem => reflect(reify(codomain), Map(id -> sem)), true)
+      yield Sem.Fun(domain, sem => reflect(reify(codomain), Map(id -> sem)))
 
     case Exp.App(operator, operand) =>
       for
-        fun @ Sem.Abs(domain, _, true) <- inferFun(operator)
+        Sem.Fun(domain, abs) <- inferFun(operator)
         _ <- check(operand, domain)
-      yield fun(reflect(operand))
+      yield abs(reflect(operand))
 
     case Exp.Var(id) =>
       ctx.get(id)
@@ -118,8 +127,9 @@ object Type extends (Exp => Option[Exp]):
         typ <- infer(body)(using ctx + (id -> arity) ++ constructors.map(_ -> reflect(_)))
       yield typ
 
+  // TODO: more checking rules
   private def check(exp: Exp, typ: Typ)(using Map[Sym, Typ]): Option[Unit] =
-    for typ1 <- infer(exp) if typ1 === typ yield ()
+    for t <- infer(exp) if t === typ yield ()
 
   def apply(exp: Exp): Option[Exp] =
     for _ <- infer(exp)(using Map.empty) yield exp
