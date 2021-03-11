b02c340825d6507f1eb37e20126ffb0dacfc7311
intsuc <intsuc@gmail.com>
2021-03-11 23:40:04 +0900

    Add annotation

--- src/main/scala/mcl/ast/Core.scala
+++ src/main/scala/mcl/ast/Core.scala
@@ -7,8 +7,9 @@ object Core with
   enum Exp with
     case Typ(level: Int)
     case Fun(domain: Exp, codomain: Exp)
-    case Abs(domain: Exp, body: Exp)
+    case Abs(body: Exp)
     case App(operator: Exp, operand: Exp)
     case Sum(variants: Seq[Exp])
-    case Inj(annotation: Exp, index: Int, target: Exp)
+    case Inj(index: Int, target: Exp)
     case Var(idx: Idx)
+    case Ann(target: Exp, annotation: Exp)
--- src/main/scala/mcl/phase/Type.scala
+++ src/main/scala/mcl/phase/Type.scala
@@ -13,10 +13,10 @@ object Type extends (Exp => Option[Exp]):
   private enum Sem with
     case Typ(level: Int)
     case Fun(domain: Sem, clo: Clo)
-    case Abs(domain: Sem, clo: Clo)
+    case Abs(clo: Clo)
     case App(operator: Sem, operand: Sem)
     case Sum(variants: Seq[Sem])
-    case Inj(annotation: Sem, index: Int, target: Sem)
+    case Inj(index: Int, target: Sem)
     case Var(lvl: Lvl)
 
   private def reflect(env: Env, exp: Exp): Sem = exp match
@@ -26,21 +26,24 @@ object Type extends (Exp => Option[Exp]):
     case Exp.Fun(domain, codomain) =>
       Sem.Fun(reflect(env, domain), Clo(env, codomain))
 
-    case Exp.Abs(domain, body) =>
-      Sem.Abs(reflect(env, domain), Clo(env, body))
+    case Exp.Abs(body) =>
+      Sem.Abs(Clo(env, body))
 
     case Exp.App(operator, operand) => (reflect(env, operator), reflect(env, operand)) match
-      case (Sem.Abs(_, clo), operand) => clo(operand)
+      case (Sem.Abs(clo), operand) => clo(operand)
       case (operator, operand) => Sem.App(operator, operand)
 
     case Exp.Sum(variants) =>
       Sem.Sum(variants.map(reflect(env, _)))
 
-    case Exp.Inj(annotation, index, target) =>
-      Sem.Inj(reflect(env, annotation), index, reflect(env, target))
+    case Exp.Inj(index, target) =>
+      Sem.Inj(index, reflect(env, target))
 
     case Exp.Var(idx) =>
       env(idx.toInt)
+
+    case Exp.Ann(target, _) =>
+      reflect(env, target)
 
   private def reify(env: Lvl, sem: Sem): Exp = sem match
     case Sem.Typ(level) =>
@@ -49,8 +52,8 @@ object Type extends (Exp => Option[Exp]):
     case Sem.Fun(domain, clo) =>
       Exp.Fun(reify(env, domain), reify(env.next, clo(Sem.Var(env))))
 
-    case Sem.Abs(domain, clo) =>
-      Exp.Abs(reify(env, domain), reify(env.next, clo(Sem.Var(env))))
+    case Sem.Abs(clo) =>
+      Exp.Abs(reify(env.next, clo(Sem.Var(env))))
 
     case Sem.App(operator, operand) =>
       Exp.App(reify(env, operator), reify(env, operand))
@@ -58,8 +61,8 @@ object Type extends (Exp => Option[Exp]):
     case Sem.Sum(variants) =>
       Exp.Sum(variants.map(reify(env, _)))
 
-    case Sem.Inj(annotation, index, target) =>
-      Exp.Inj(reify(env, annotation), index, reify(env, target))
+    case Sem.Inj(index, target) =>
+      Exp.Inj(index, reify(env, target))
 
     case Sem.Var(lvl) =>
       Exp.Var(lvl.toIdx(env))
@@ -71,8 +74,8 @@ object Type extends (Exp => Option[Exp]):
     case (Sem.Fun(domain1, clo1), Sem.Fun(domain2, clo2)) =>
       conv(env, domain1, domain2) && conv(env.next, clo1(Sem.Var(env)), clo2(Sem.Var(env)))
 
-    case (Sem.Abs(domain1, clo1), Sem.Abs(domain2, clo2)) =>
-      conv(env, domain1, domain2) && conv(env.next, clo1(Sem.Var(env)), clo2(Sem.Var(env)))
+    case (Sem.Abs(clo1), Sem.Abs(clo2)) =>
+      conv(env.next, clo1(Sem.Var(env)), clo2(Sem.Var(env)))
 
     case (Sem.App(operator1, operand1), Sem.App(operator2, operand2)) =>
       conv(env, operator1, operator2) && conv(env, operand1, operand2)
@@ -80,8 +83,8 @@ object Type extends (Exp => Option[Exp]):
     case (Sem.Sum(variants1), Sem.Sum(variants2)) =>
       (variants1 zip variants2).forall(conv(env, _, _))
 
-    case (Sem.Inj(annotation1, index1, target1), Sem.Inj(annotation2, index2, target2)) =>
-      index1 == index2 && conv(env, annotation1, annotation2) && conv(env, target1, target2)
+    case (Sem.Inj(index1, target1), Sem.Inj(index2, target2)) =>
+      index1 == index2 && conv(env, target1, target2)
 
     case (Sem.Var(lvl1), Sem.Var(lvl2)) =>
       lvl1 == lvl2
@@ -92,13 +95,13 @@ object Type extends (Exp => Option[Exp]):
   private type Typ = Sem
 
   private final case class Ctx(sems: Env, typs: Vector[Typ]) with
-    def size: Int = sems.size
+    def toLvl: Lvl = Lvl(sems.size)
 
   private object Ctx with
     def empty: Ctx = Ctx(Vector.empty, Vector.empty)
 
   extension (typ: Typ) private def +: (ctx: Ctx): Ctx =
-    Ctx(Sem.Var(Lvl(ctx.size)) +: ctx.sems, typ +: ctx.typs)
+    Ctx(Sem.Var(ctx.toLvl) +: ctx.sems, typ +: ctx.typs)
 
   private def inferTyp(ctx: Ctx, exp: Exp): Option[Int] =
     for Sem.Typ(level) <- infer(ctx, exp) yield level
@@ -113,12 +116,8 @@ object Type extends (Exp => Option[Exp]):
         codomainLevel <- inferTyp(reflect(ctx.sems, domain) +: ctx, codomain)
       yield Sem.Typ(domainLevel max codomainLevel)
 
-    case Exp.Abs(domain, body) =>
-      for
-        _ <- inferTyp(ctx, domain)
-        domain <- Some(reflect(ctx.sems, domain))
-        body <- infer(domain +: ctx, body)
-      yield Sem.Fun(domain, Clo(ctx.sems, reify(Lvl(ctx.size), body)))
+    case Exp.Abs(body) =>
+      ??? // TODO: error message
 
     case Exp.App(operator, operand) =>
       for
@@ -135,19 +134,34 @@ object Type extends (Exp => Option[Exp]):
         maxLevel <- levels.maxOption
       yield Sem.Typ(maxLevel)
 
-    case Exp.Inj(annotation, index, target) =>
-      for
-        _ <- inferTyp(ctx, annotation)
-        typ @ Sem.Sum(variants) = reflect(ctx.sems, annotation)
-        variant <- variants.lift(index)
-        _ <- check(ctx, target, variant)
-      yield typ
+    case Exp.Inj(index, target) =>
+      ??? // TODO: error message
 
     case Exp.Var(idx) =>
       Some(ctx.typs(idx.toInt))
 
-  private def check(ctx: Ctx, exp: Exp, typ: Typ): Option[Unit] =
-    for t <- infer(ctx, exp) if conv(Lvl(ctx.size), t, typ) yield ()
+    case Exp.Ann(target, annotation) =>
+      for
+        _ <- inferTyp(ctx, annotation)
+        annotation <- Some(reflect(ctx.sems, annotation))
+        _ <- check(ctx, target, annotation)
+      yield annotation
+
+  private def check(ctx: Ctx, exp: Exp, typ: Typ): Option[Unit] = (exp, typ) match
+    case (Exp.Abs(body), Sem.Fun(domain, codomain)) =>
+      check(domain +: ctx, body, codomain(Sem.Var(ctx.toLvl)))
+
+    case (Exp.Inj(index, target), Sem.Sum(variants)) =>
+      for
+        variant <- variants.lift(index)
+        _ <- check(ctx, target, variant)
+      yield ()
+
+    case (exp, typ) =>
+      for
+        t <- infer(ctx, exp)
+        if conv(ctx.toLvl, t, typ)
+      yield ()
 
   def apply(exp: Exp): Option[Exp] =
     for _ <- infer(Ctx.empty, exp) yield exp
