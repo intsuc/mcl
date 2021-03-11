32b0b7568b7ba988eef172f400a3e475e6eee579
intsuc <intsuc@gmail.com>
2021-03-11 18:47:06 +0900

    Add sum types

--- src/main/scala/mcl/ast/Core.scala
+++ src/main/scala/mcl/ast/Core.scala
@@ -2,10 +2,13 @@ package mcl.ast
 
 import Indices.Idx
 
+// TODO: eliminator for sum types
 object Core with
   enum Exp with
     case Typ(level: Int)
     case Fun(domain: Exp, codomain: Exp)
     case Abs(domain: Exp, body: Exp)
     case App(operator: Exp, operand: Exp)
+    case Sum(variants: Seq[Exp])
+    case Inj(annotation: Exp, index: Int, target: Exp)
     case Var(idx: Idx)
